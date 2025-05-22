cb24c7e97b3754462f647b3382650b095b85551e
setya31 <79081950+setya31@users.noreply.github.com>
2025-05-22 19:56:26 +0700

    Menambahkan TP 8

---
+++ 08_Runtime_Configuration_dan_Internationalization/TP/tpmodul8_2311104050/tpmodul8_2311104050/CovidConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; } = "celcius";
+    public int batas_hari_deman { get; set; } = 14;
+    public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+    public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+    public void LoadConfig(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            var json = File.ReadAllText(filePath);
+            var config = JsonConvert.DeserializeObject<CovidConfig>(json);
+            satuan_suhu = config.satuan_suhu;
+            batas_hari_deman = config.batas_hari_deman;
+            pesan_ditolak = config.pesan_ditolak;
+            pesan_diterima = config.pesan_diterima;
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu == "celcius")
+        {
+            satuan_suhu = "fahrenheit";
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+        }
+    }
+}
---
+++ 08_Runtime_Configuration_dan_Internationalization/TP/tpmodul8_2311104050/tpmodul8_2311104050/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        string configFilePath = "covid_config.json";
+        config.LoadConfig(configFilePath);
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+        int hariDeman = Convert.ToInt32(Console.ReadLine());
+
+        bool isSuhuValid = (config.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+                           (config.satuan_suhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5);
+        bool isHariValid = hariDeman < config.batas_hari_deman;
+
+        if (isSuhuValid && isHariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        // Contoh penggunaan method UbahSatuan
+        config.UbahSatuan();
+        Console.WriteLine($"Satuan suhu telah diubah menjadi: {config.satuan_suhu}");
+    }
+}
