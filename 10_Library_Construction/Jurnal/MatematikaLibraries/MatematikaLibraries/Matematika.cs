42ee7829aef97f4e19e32c56bf0c0fbc96ed7056
setya31 <79081950+setya31@users.noreply.github.com>
2025-05-31 15:20:41 +0700

    Menambahkan Jurnal Modul 10

---
+++ 10_Library_Construction/Jurnal/MatematikaLibraries/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,60 @@
+﻿namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public static int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public static string Turunan(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            int pangkat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koef = persamaan[i] * pangkat;
+                if (koef == 0) continue;
+
+                string suku = koef.ToString();
+                if (pangkat - 1 > 1)
+                    suku += $"x{pangkat - 1}";
+                else if (pangkat - 1 == 1)
+                    suku += "x";
+
+                hasil.Add(suku);
+                pangkat--;
+            }
+            return string.Join(" + ", hasil).Replace("+ -", "- ");
+        }
+
+        public static string Integral(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            int pangkat = persamaan.Length;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                double koef = (double)persamaan[i] / (pangkat);
+                string suku = (koef == 1 ? "" : koef.ToString("0.##")) + $"x{pangkat}";
+                hasil.Add(suku);
+                pangkat--;
+            }
+
+            hasil.Add("C");
+            return string.Join(" + ", hasil).Replace("+ -", "- ");
+        }
+    }
+}
---
+++ 10_Library_Construction/Jurnal/MatematikaLibraries/modul10_2311104050/Program.cs
@@ -0,0 +1,18 @@
+﻿using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"FPB: {Matematika.FPB(60, 45)}");     // Output: 15
+        Console.WriteLine($"KPK: {Matematika.KPK(12, 8)}");       // Output: 24
+
+        int[] fungsiTurunan = { 1, 4, -12, 9 };
+        Console.WriteLine("Turunan: " + Matematika.Turunan(fungsiTurunan));
+        // Output: 3x2 + 8x -12
+
+        int[] fungsiIntegral = { 4, 6, -12, 9 };
+        Console.WriteLine("Integral: " + Matematika.Integral(fungsiIntegral));
+        // Output: x4 + 2x3 - 6x2 + 9x + C
+    }
+}
