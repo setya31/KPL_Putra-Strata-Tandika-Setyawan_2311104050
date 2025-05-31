a794fd1bb11caf8b65c56a27ffdea5a10a8f9b70
setya31 <79081950+setya31@users.noreply.github.com>
2025-05-31 14:39:15 +0700

    Menambahkan Jurnal 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/Jurnal/modul9_2311104050/modul9_2311104050/Movie.cs
@@ -0,0 +1,7 @@
+﻿public class Movie
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public List<string> Stars { get; set; }
+    public string Description { get; set; }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/Jurnal/modul9_2311104050/modul9_2311104050/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MoviesController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>
+    {
+        new Movie
+        {
+            Title = "The Shawshank Redemption",
+            Director = "Frank Darabont",
+            Stars = new List<string>{ "Tim Robbins", "Morgan Freeman" },
+            Description = "Two imprisoned men bond over a number of years..."
+        },
+        new Movie
+        {
+            Title = "The Godfather",
+            Director = "Francis Ford Coppola",
+            Stars = new List<string>{ "Marlon Brando", "Al Pacino" },
+            Description = "The aging patriarch of an organized crime dynasty..."
+        },
+        new Movie
+        {
+            Title = "The Dark Knight",
+            Director = "Christopher Nolan",
+            Stars = new List<string>{ "Christian Bale", "Heath Ledger" },
+            Description = "When the menace known as the Joker emerges..."
+        }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Movie>> Get() => movies;
+
+    [HttpGet("{id}")]
+    public ActionResult<Movie> Get(int id) => movies.ElementAtOrDefault(id);
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Movie m)
+    {
+        movies.Add(m);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        if (id >= 0 && id < movies.Count)
+        {
+            movies.RemoveAt(id);
+            return Ok();
+        }
+        return NotFound();
+    }
+} 
