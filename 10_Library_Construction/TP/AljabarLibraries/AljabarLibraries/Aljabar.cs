97c7b7dad4354e568503bcf0165c2d190e1778bb
setya31 <79081950+setya31@users.noreply.github.com>
2025-05-31 15:06:58 +0700

    Menambahkan TP Modul 10

---
+++ 10_Library_Construction/TP/AljabarLibraries/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,26 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double x2 = a * a;
+            double x1 = 2 * a * b;
+            double konstanta = b * b;
+            return new double[] { x2, x1, konstanta };
+        }
+    }
+}
---
+++ 10_Library_Construction/TP/AljabarLibraries/tpmodul10/Program.cs
@@ -0,0 +1,13 @@
+﻿using AljabarLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine($"Akar: {akar[0]}, {akar[1]}");
+
+        double[] hasil = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine($"Hasil Kuadrat: {hasil[0]}x^2 + {hasil[1]}x + {hasil[2]}");
+    }
+}
